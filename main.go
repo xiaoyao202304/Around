83e7c28989de2047c5dda7ae8acbf21901146a4a
xiaoyao202304 <xiaoyao202304@gmail.com>
2018-06-09 09:08:10 -0700

    change ip address

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.196.161.40:9200"
+	ES_URL = "http://35.237.135.98:9200"
 )
 
 func main() {
